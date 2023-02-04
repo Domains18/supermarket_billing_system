f0dc973efb29dc04afbbfc290c1ef674684a2b28
Domains18 <gibsonsgibson88@gmail.com>
2023-02-04 21:07:32 +0300

    supermarket billing system using c++

--- billing1.cpp
+++ billing1.cpp
@@ -1,20 +1,74 @@
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
-class shopping{
-    private:
-        int pcode;
-        float price;
-        float dis;
-        string pname
+class shopping
+{
+private:
+    int pcode;
+    float price;
+    float dis;
+    string pname;
 
+public:
+    void menu();
+    void administrator();
+    void buyer();
+    void add();
+    void edit();
+    void rem();
+    void list();
+    void receipt();
+};
 
-    public:
-        void menu();
-        void administrator();
-        void buyer();
-        void add();
-        void edit();
+void shopping ::menu()
+{
+    int choice;
+    string email;
+    string password;
+
+    cout << "\t\t\t\t------------------------------------------------------\n";
+    cout << "\t\t\t\t--------------SuperMarket System Menu------------------\n";
+    cout << "\t\t\t\t------------------------------------------------------\n";
+
+    cout << "\t\t\t\t 1) Administrator|\n";
+    cout << "\t\t\t\t                 |\n";
+    cout << "\t\t\t\t 2) Client|\n";
+    cout << "\t\t\t\t                 |\n";
+    cout << "\t\t\t\t 3) Exit         | \n";
+    cout << "\t\t\t\t                 |\n";
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        cout << "\t\t\t\t Please Login \n";
+        cout << "\t\t\t\t Enter Email \n";
+        cin >> email;
+        cout << " Enter Your Password .....";
+        cin >> password;
+
+        if (email = "admin@gmail" &&password = "admin")
+        {
+            administrator();
+        }
+        else
+        {
+            cout << "Invalid username or password";
+        }
+        break;
+
+    case 2:
+    {
+        buyer();
+    }
+    case 3:
+    {
+        exit(0);
+    }
+    default:{
+        cout << "enter a valid choice";
+    }
+    }
 }
