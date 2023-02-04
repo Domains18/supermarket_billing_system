1c1e41b24c10f2a47022aa181cfaf54763d0c495
Domains18 <gibsonsgibson88@gmail.com>
2023-02-04 20:29:01 +0300

    supermarket billing system using c++

---
+++ billing1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+class shopping{
+    private:
+        int pcode;
+        float price;
+        float dis;
+        string pname
+
+
+    public:
+        void menu();
+        void administrator();
+        void buyer();
+        void add();
+        void edit();
+}
