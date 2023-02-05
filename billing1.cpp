3cf61565145001d64fa99e04ae1269ca091670d2
Domains18 <gibsonsgibson88@gmail.com>
2023-02-05 11:12:03 +0300

    supermarket billing system using c++

--- billing1.cpp
+++ billing1.cpp
@@ -170,5 +170,122 @@ while(f.read((char*)&s, sizeof(supermarket)))
 f.close();
 }
 
+void supermarket::PlaceOrder(){
+int order[50], quan[50], c = 0;
+float amt, damt, total = 0;
+char ch = 'Y';
+Menu();
+cout<<"\n============================";
+cout<<"\n PLACE YOUR ORDER";
+cout<<"\n============================\n";
+do{
+        cout << "\n\nEnter The Product number : ";
+        cin >> order[c];
+        cout << "\nQuality in number : ";
+        cin >> quan[c];
+        c++;
+        cout << "\n\nDo you want to order Another Product?: ";
+        cin >> ch;
+} while (ch == 'y' || ch == 'Y');
+cout << "\n\nThank You for placing the order";
+system("clear");
+cout<<"\n\n*********************INVOICE*********\n";
+cout<<"\nItem No.\tItem Name\tQuantity \tPrice \tAmount \tAmount after discount\n";
+for (int x = 0; x = c;x++){
+        f.open("home/supermarket_billing_system/Store.txt", ios::in);
+        f.read((char *)&s, sizeof(supermarket));
+        while(!f.eof()){
+            if(itemNo==order[x]){
+                amt = s.price * quan[x];
+                damt = amt - (amt * s.dis / 100);
+                cout << "\n"
+                     << order[x] << "\t" << s.name << "\t" << quan[x] << "\t\t"
+                     << "\t\t" << s.price << "\t" << amt << "\t\t" << damt;
+                total += damt;
+            }
+            f.read((char *)&s, sizeof(supermarket));
+        }
+        f.close();
+    }
+    cout << "\n\n\t\t\t\t\tTotal = " << total;
+}
 
-
+void supermarket::intro(){
+    system("clear");
+    cout << "\t\t\t\t";
+    cout << "BILLING";
+    cout << "\t\t\t\t";
+    cout << "PROJECT";
+    cout << "\n Made By: Gibsons Gibson";
+}
+void supermarket::AdminMenu(){
+    system("clear");
+    char ch2;
+    cout << "\n\n\n\tADMIN MENU";
+    cout << "\n\n\t1.CREATE PRODUCT";
+    cout << "\n\n\t2.DISPLAY ALL PRODUCTS";
+    cout << "\n\n\t3.QUERY ";
+    cout << "\n\n\t4.MODIFY PRODUCT";
+    cout << "\n\n\t5.DELETE PRODUCT";
+    cout << "\n\n\t6.VIEW PRODUCT MENU";
+    cout << "\n\n\t7.BACK TO MAIN MENU";
+    cout << "\n\n\tPlease Enter Your Choice (1-7) ";
+    cin >> ch2;
+    switch(ch2){
+        case '1':
+            system("clear");
+            AddProduct();
+            break;
+        case '2':
+            Display();
+            break;
+        case '3':
+            int num;
+            system("clear");
+            cout << "\n\n\tPlease Enter The Product No. ";
+            cin >> num;
+            DisplaySpecific(num);
+            break;
+        case '4':
+            Update();
+            break;
+        case '5':
+            Delete();
+            break;
+        case '6':
+            Menu();
+            break;
+        case '7':
+            break;
+        default:
+            cout << "\a";
+            AdminMenu();
+    }
+}
+int main(){
+    char ch;
+    s.intro();
+    do{
+            cout << "\n\n\n\tMAIN MENU";
+            cout << "\n\n\n\t01. CUSTOMER";
+            cout << "\n\n\n\t03. EXIT";
+            cout << "\n\n\tPlease Select Your Option (1-3)";
+            cin >> ch;
+            switch(ch){
+                case '1': system("clear");
+                    s.PlaceOrder();
+                    break;
+                
+                case '2' :
+                    s.AdminMenu();
+                    break;
+                
+                case '3' :
+                    exit(0);
+                    default :
+                    cout << "\a";
+                    
+            } 
+        }while(ch!='3');
+            return 0;
+    }
